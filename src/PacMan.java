e14fbdf85051720733bcfce4a1ff4455705d8e04
TheGermanDog17 <gav2424bb@gmail.com>
2025-04-10 23:19:57 -0400

    Add image assets for game characters and elements

--- src/PacMan.java
+++ src/PacMan.java
@@ -12,8 +12,25 @@ public class PacMan extends JPanel {
     private int boardWidth = columnCount * tileSize;
     private int boardHeight = rowCount * tileSize;
 
+    private Image wallImage;
+    private Image blueGhostImage;
+    private Image orangeGhostImage;
+    private Image pinkGhostImage;
+    private Image redGhostImage;
+
+    private Image pacManUpImage;
+    private Image pacManDownImage;
+    private Image pacManLeftImage;
+    private Image pacManRightImage;
+
     PacMan() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+
+        // Load Images
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+        
     }
 }
